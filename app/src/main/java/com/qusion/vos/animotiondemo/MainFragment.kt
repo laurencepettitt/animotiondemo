8507e08d9cc1f4090fb4cd563668e6707a5aaf79
michona <miki@qusion.com>
2020-09-02 07:11:18 +0200

    Cleans up dependencies (external).

---
+++ app/src/main/java/com/qusion/vos/animotiondemo/MainFragment.kt
@@ -0,0 +1,9 @@
+package com.qusion.vos.animotiondemo
+
+import com.qusion.vos.animotiondemo.base.BaseFragment
+import com.qusion.vos.animotiondemo.databinding.MainFragmentBinding
+
+class MainFragment : BaseFragment<MainFragmentBinding>(R.layout.main_fragment) {
+    override fun onBind() {
+    }
+}
